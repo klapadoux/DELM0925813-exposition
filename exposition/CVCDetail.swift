e69d0cdb97413a370b3cf4ec850d791dd50d8121
etudiant <etudiant@LD140-M13-02810.local>
2015-10-20 22:13:14 -0400

    Quasiment tout

--- exposition/CVCDetail.swift
+++ exposition/CVCDetail.swift
@@ -9,30 +9,22 @@
 import UIKit
 
 class CVCDetail: UIViewController {
-
+    var listeDetail:Array<String> = Array()
+    
+    @IBOutlet weak var afficheDetail: UIImageView!
+    @IBOutlet weak var texteDetail: UITextView!
+    @IBOutlet weak var titreDetail: UILabel!
+    
     @IBAction func btRetour(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titreDetail.text = listeDetail[0]
+        afficheDetail.image = UIImage(named: listeDetail[1])
+        texteDetail.text = listeDetail[2]
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- exposition/ViewController.swift
+++ exposition/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UICollectionViewDataSource{
     
     var listeAffiches:Array<Array<String>> = Array()
     
+    @IBOutlet weak var cvAffiche: UICollectionView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,11 +21,6 @@ class ViewController: UIViewController, UICollectionViewDataSource{
         let pathFichierPlist = NSBundle.mainBundle().pathForResource("lesDonnees", ofType: "plist")!
         listeAffiches = NSArray(contentsOfFile: pathFichierPlist) as! Array<Array<String>>
         println(listeAffiches)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -33,11 +30,26 @@ class ViewController: UIViewController, UICollectionViewDataSource{
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let celluleCourante = collectionView.dequeueReusableCellWithReuseIdentifier("CellulleAffiche", forIndexPath:indexPath) as! CVCAffiche
+
+        celluleCourante.titre.text = listeAffiches[indexPath.row][0]
         
+        celluleCourante.affiche.image = UIImage(named: listeAffiches[indexPath.row][1])
+        
+    
         return celluleCourante
         
     } // collectionView: cellForItemAtIndexPath
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let selection = cvAffiche.indexPathForCell(sender as! UICollectionViewCell)!.row
+        
+        var destination = segue.destinationViewController as! CVCDetail
+        
+        destination.listeDetail = listeAffiches[selection]
+        
+        //destination.details = amis[selection]
+    }
 
 }
 
